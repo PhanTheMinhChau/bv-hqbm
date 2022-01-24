d483f0bf00207114e0d1837b562ab9468f7ab25c
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2022-01-24 14:35:46 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,18 @@
+import subprocess as sub
+from time import sleep
+import random
+
+#38-686
+f = open("C:/Users/phanc/Desktop/hqbm/mess.txt", "r", encoding='utf-8')
+a = f.readlines()
+f.close()
+for i in range(len(a)):
+    a[i] = a[i].replace("\n", "")
+while True:
+    h = random.choice(a)
+    sub.run("adb shell input tap 38 686")
+    #sub.run("adb shell input tap 38 686")
+    sub.run("adb shell input tap 38 686;input tap 250 1540;am broadcast -a ADB_INPUT_TEXT --es msg '%s';input tap 666 1481" %h)
+    #sub.run("adb shell am broadcast -a ADB_INPUT_TEXT --es msg '%s'" % h)
+    #sub.run("adb shell input tap 666 1481")
+    sleep(0.000001)
